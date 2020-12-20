c90fce76e9049521cd25e8c72f0d45328c0332ef
tobiashahn <tobias.hahn@dlh.de>
2020-12-20 23:06:16 +0000

    Deploying to gh-pages from master @ 6a99331caa09992446c9edec677ebbd885b1914f 🚀

---
+++ tilesets/misc_office.tsx
@@ -0,0 +1,24 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="misc_office" tilewidth="32" tileheight="32" tilecount="256" columns="16">
+ <image source="../../lag-workadventure/lag-raunheim/tilesets_deviant_milkian_1.png" width="512" height="512"/>
+ <tile id="179">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="195">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="211">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="227">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+</tileset>
